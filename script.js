73c447330c9c938d2616b84f0de3e30baac87805
Developer-Umarbek <umarbektugalov@gmail.com>
2022-08-14 14:11:51 +0500

    JS tutorial

---
+++ script.js
@@ -0,0 +1,216 @@
+
+// ==================== 1-m =====================
+
+// let L  = +prompt("Uzunlikni Kiriting (sm)");
+
+// let yaxlit = L / 100;
+
+// console.log(yaxlit +  ' Metr');
+
+// alert(yaxlit +  ' Metr')
+
+
+// =============== 2-m ======================
+
+// let M = +prompt("Og'irlik Yozing (Tonna))");
+
+// let result = M * 1000 ;
+
+// alert( M  +  " Tonna = "  +  result +  ' kg');
+
+
+//  =============== 4-m ====================
+
+// let a = +prompt("Ikki xonali sonni kiriting")
+
+// let b = a%10
+
+// let d = a/10
+
+// alert(b + " Bir xonali son")
+
+// alert(Math.floor(d)+'0' + " O'n xonali son")
+
+
+// ============ 5-M =================
+
+
+// let a = +prompt("Ikki xonali sonni kiriting");
+
+// let b = a%10;
+
+// let d = a/10;
+
+// alert(b + d)
+
+
+// ============= 6-m ===============
+
+// let a = +prompt("Ikki xonali sonni kiriting");
+
+// let b = a%10;
+
+// let d = a/10;
+
+// let c = b * d;
+
+// alert(Math.floor(c))
+
+
+
+// 1-dars Mantiqiy amallar (if , else)
+
+
+// ============ 1-m =================
+
+// let A = +prompt("Butun son kiriting !")
+
+// if (A < 1) {
+//      alert("Bu butun son emas !")
+// }else{
+//    alert("Bu butun son !")
+// }
+
+
+// ============== 2-m ============
+
+// let A = +prompt("Butun son kiriting !")
+
+// if (A > 1) {
+//      alert("Bu butun son !")
+// }else{
+//    alert("Bu butun son emas !")
+// }
+
+// ============ 3-m ============
+
+// let a = +prompt("Juft son kiriting ! ");
+
+// if (a >= 10) {
+//     alert("Bu juft son !")
+// }else{
+//     alert("Bu juft son emas !")
+// }
+
+
+// ============ 4-m ==============
+//  let a = 15
+//  let b = 25
+
+//  if (a > 0 && b > 0) {
+//     alert('true')
+//  }else{
+//     alert('true')
+//  }
+
+// =============== 5-m ============
+
+// let a = 25 
+// let b = 15
+// let c = 10
+
+// if (a > b && b > c) {
+//     alert('True')
+// }else{
+//     alert('False')
+// }
+
+
+// ============== 6-m ==============
+
+// let a = 4
+// let b = 7
+// let c = 10
+
+// if (a < b || b > c) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+// ========== 7-m ===============
+
+// let a = 5;
+
+// let c = a / 2
+// console.log(c + " Toq son emas");
+
+
+// =========== 8-m ============
+
+// let a = 5
+// let b = 8
+// let c = 10
+// let d = 15
+
+// console.log("Barchsi musbat");
+   
+
+// ============ 10-m ===========
+
+// let a = +prompt("Hafta raqamini yozing !")
+
+// if (a == 1) {
+//     alert("Dushanba")
+// }else if (a == 2) {
+//     alert("Seshanba")
+// }else if (a == 3){
+//     alert("Chorshanba")
+// }else if (a == 4){
+//     alert("Payshanba")
+// }else if (a == 5) {
+//     alert("Juma")
+// }else if (a == 6) {
+//     alert("Shanba")
+// }else if (a == 7) {
+//     alert("Yakshanba")
+// }
+
+// console.log(a);
+
+
+
+// =========== 11-m ==============
+
+// let num = +prompt("Tel kodingizni kiriting") ;
+
+// if (num == 90 || num == 91) {
+//     alert("Bu 'Beeline' kompaniyasi ")
+// }else if (num == 93 || num == 94) {
+//     alert("Bu 'Ucell' kompaniyasi")
+// }else if (num == 99){
+//     alert("Bu 'Uzmobile' kompaniyasi")
+// }
+
+
+// =========== 14-m ==============
+
+// let a = +prompt("Butun son kiriting")
+// let b = +prompt("2-butun sonni kiriting")
+
+// let c = Math.max(a , b);
+// alert(c + " bu katta son")
+
+
+// ========== 15-m =================
+
+// let a = +prompt("Butun son kiriting")
+// let b = +prompt("2-butun sonni kiriting")
+// let d = +prompt("3-butun sonni kiriting")
+
+// let c = Math.max(a , b , d);
+// alert(c + " bu katta son")
+
+// ============= 16-m =============
+
+// let a = +prompt("Butun son kiriting")
+// let b = +prompt("2-butun sonni kiriting")
+// let d = +prompt("3-butun sonni kiriting")
+
+// let c = Math.min(a , b , d);
+// alert(c + " bu kichik son")
+
+
+
